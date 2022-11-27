d41b9de4478e704126491b1a1d37262de44a46a2
ckosten <kosten.catherine@gmail.com>
2022-11-27 11:38:11 +0100

    Delete src directory

--- src/tools/query_analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-# line 1 points
-data_enrichedKG=pd.read_csv('experiments/SparqlPrediction/KG_enriched_ont.csv')
-data_enrichedKG['COUNTER'] =1
-group_data_baselineKG = data_enrichedKG.groupby(['database','score'])['COUNTER'].sum().reset_index()
-
-
